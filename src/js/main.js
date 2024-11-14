7d04996b09bb2fb2f09a853a1ecaba3ae262dae0
tealtourmaline <softivyad@gmail.com>
2024-11-14 16:15:53 -0500

    funcionalidad completa de la tienda (faltan estilos)

--- src/js/main.js
+++ src/js/main.js
@@ -80,7 +80,22 @@ function init() {
     btns["btn_volver2"] = document.getElementById("btn_volver2");
     btns["btn_volver3"] = document.getElementById("btn_volver3");
     btns["btn_continuar"] = document.getElementById("btn_continuar");
+
+    //botones de las mejoras
     btns["granjaPapasBtn"] = document.getElementById("granjaPapasBtn");
+    btns["fabricaSalchichasBtn"] = document.getElementById("fabricaSalchichasBtn");
+    btns["criaderoCodornicesBtn"] = document.getElementById("criaderoCodornicesBtn");
+    btns["casaCerdosBtn"] = document.getElementById("casaCerdosBtn");
+    btns["lecheriaBtn"] = document.getElementById("lecheriaBtn");
+    btns["cultivoMaizBtn"] = document.getElementById("cultivoMaizBtn");
+    btns["fabricaEmulsionBtn"] = document.getElementById("fabricaEmulsionBtn");
+    btns["maquinaHeladosBtn"] = document.getElementById("maquinaHeladosBtn");
+    btns["plantacionTomatesBtn"] = document.getElementById("plantacionTomatesBtn");
+    btns["plantaNuclearBtn"] = document.getElementById("plantaNuclearBtn");
+    btns["fabricaTarroBtn"] = document.getElementById("fabricaTarroBtn");
+    btns["laboratorioNatillaBtn"] = document.getElementById("laboratorioNatillaBtn");
+    btns["fabricaTejidosBtn"] = document.getElementById("fabricaTejidosBtn");
+    btns["mirellaBtn"] = document.getElementById("mirellaBtn");
     
     setTimeout(() => {
       if (nombre_salch) {
@@ -95,6 +110,7 @@ function init() {
 }
 
 function asignarEventosMenu() {
+    //eventos de navegación
     //btns["btn_juego"].addEventListener("click", () => cargarSeccion("juego"));
     btns["btn_volver"].addEventListener("click", () => cargarSeccion("juego"));
     btns["btn_volver2"].addEventListener("click", () => cargarSeccion("juego"));
@@ -106,7 +122,21 @@ function asignarEventosMenu() {
     btns["btn_puntuacion"].addEventListener("click", () => cargarSeccion("puntuacion"));
     btns["btn_creditos"].addEventListener("click", () => cargarSeccion("creditos"));
 
+    //eventos de mejoras
     btns["granjaPapasBtn"].addEventListener("click", () => comprarGranjaPapas());
+    btns["fabricaSalchichasBtn"].addEventListener("click", () => comprarFabricaSalchichas());
+    btns["criaderoCodornicesBtn"].addEventListener("click", () => comprarCriaderoCodornices());
+    btns["casaCerdosBtn"].addEventListener("click", () => comprarCasaCerdos());
+    btns["lecheriaBtn"].addEventListener("click", () => comprarLecheria());
+    btns["cultivoMaizBtn"].addEventListener("click", () => comprarCultivoMaiz());
+    btns["fabricaEmulsionBtn"].addEventListener("click", () => comprarFabricaEmulsion());
+    btns["maquinaHeladosBtn"].addEventListener("click", () => comprarMaquinaHelados());
+    btns["plantacionTomatesBtn"].addEventListener("click", () => comprarPlantacionTomates());
+    btns["plantaNuclearBtn"].addEventListener("click", () => comprarPlantaNuclear());
+    btns["fabricaTarroBtn"].addEventListener("click", () => comprarFabricaTarro());
+    btns["laboratorioNatillaBtn"].addEventListener("click", () => comprarLaboratorioNatilla());
+    btns["fabricaTejidosBtn"].addEventListener("click", () => comprarFabricaTejidos());
+    btns["mirellaBtn"].addEventListener("click", () => comprarMirella());
 }
 
 function ocultar() {
@@ -221,6 +251,45 @@ function verificarDesbloqueos(){
   if(totalSalchipapas >= 10){
     document.getElementById("granjaPapasBtn").disabled = false;
   }
+  if(totalSalchipapas >= 85){
+    document.getElementById("fabricaSalchichasBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 237){
+    document.getElementById("criaderoCodornicesBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 465){
+    document.getElementById("casaCerdosBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 769){
+    document.getElementById("lecheriaBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 1149){
+    document.getElementById("cultivoMaizBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 1605){
+    document.getElementById("fabricaEmulsionBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 2137){
+    document.getElementById("maquinaHeladosBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 2745){
+    document.getElementById("plantacionTomatesBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 3429){
+    document.getElementById("plantaNuclearBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 4189){
+    document.getElementById("fabricaTarroBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 5025){
+    document.getElementById("laboratorioNatillaBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 5937){
+    document.getElementById("fabricaTejidosBtn").disabled = false;
+  }
+  if(totalSalchipapas >= 6937){
+    document.getElementById("mirellaBtn").disabled = false;
+  }
 }
 
 
@@ -250,7 +319,149 @@ function comprarGranjaPapas(){
     cant_granja_de_papas++;
     cst_granja_de_papas += 0.5;
     document.getElementById("granjaPapasCosto").innerText = cst_granja_de_papas;
-    console.log("compró una granja de papas");
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarFabricaSalchichas(){
+  if(currentSalchipapas >= cst_fabrica_de_salchichas){
+    currentSalchipapas -= cst_fabrica_de_salchichas;
+    cant_fabrica_de_salchichas++;
+    cst_fabrica_de_salchichas += 1;
+    document.getElementById("fabricaSalchichasCosto").innerText = cst_fabrica_de_salchichas;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarCriaderoCodornices(){
+  if(currentSalchipapas >= cst_criadero_de_codornices){
+    currentSalchipapas -= cst_criadero_de_codornices;
+    cant_criadero_de_codornices++;
+    cst_criadero_de_codornices += 1.5;
+    document.getElementById("criaderoCodornicesCosto").innerText = cst_criadero_de_codornices;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarCasaCerdos(){
+  if(currentSalchipapas >= cst_casa_de_cerdos){
+    currentSalchipapas -= cst_casa_de_cerdos;
+    cant_casa_de_cerdos++;
+    cst_casa_de_cerdos += 2;
+    document.getElementById("casaCerdosCosto").innerText = cst_casa_de_cerdos;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarLecheria(){
+  if(currentSalchipapas >= cst_lecheria){
+    currentSalchipapas -= cst_lecheria;
+    cant_lecheria++;
+    cst_lecheria += 2;
+    document.getElementById("lecheriaCosto").innerText = cst_lecheria;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarCultivoMaiz(){
+  if(currentSalchipapas >= cst_cultivo_de_maiz_genetico){
+    currentSalchipapas -= cst_cultivo_de_maiz_genetico;
+    cant_cultivo_de_maiz_genetico++;
+    cst_cultivo_de_maiz_genetico += 2.5;
+    document.getElementById("cultivoMaizCosto").innerText = cst_cultivo_de_maiz_genetico;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarFabricaEmulsion(){
+  if(currentSalchipapas >= cst_fabrica_de_emulsion_de_scott){
+    currentSalchipapas -= cst_fabrica_de_emulsion_de_scott;
+    cant_fabrica_de_emulsion_de_scott++;
+    cst_fabrica_de_emulsion_de_scott += 3;
+    document.getElementById("fabricaEmulsionCosto").innerText = cst_fabrica_de_emulsion_de_scott;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarMaquinaHelados(){
+  if(currentSalchipapas >= cst_maquinas_de_helados_de_chicle){
+    currentSalchipapas -= cst_maquinas_de_helados_de_chicle;
+    cant_maquinas_de_helados_de_chicle++;
+    cst_maquinas_de_helados_de_chicle += 3.5;
+    document.getElementById("maquinaHeladosCosto").innerText = cst_maquinas_de_helados_de_chicle;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarPlantacionTomates(){
+  if(currentSalchipapas >= cst_plantacion_de_tomates){
+    currentSalchipapas -= cst_plantacion_de_tomates;
+    cant_plantacion_de_tomates++;
+    cst_plantacion_de_tomates += 4;
+    document.getElementById("plantacionTomatesCosto").innerText = cst_plantacion_de_tomates;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarPlantaNuclear(){
+  if(currentSalchipapas >= cst_planta_nuclear_de_uranio_235u){
+    currentSalchipapas -= cst_planta_nuclear_de_uranio_235u;
+    cant_planta_nuclear_de_uranio_235u++;
+    cst_planta_nuclear_de_uranio_235u += 4.5;
+    document.getElementById("plantaNuclearCosto").innerText = cst_planta_nuclear_de_uranio_235u;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarFabricaTarro(){
+  if(currentSalchipapas >= cst_fabrica_de_tarro_carmesi){
+    currentSalchipapas -= cst_fabrica_de_tarro_carmesi;
+    cant_fabrica_de_tarro_carmesi++;
+    cst_fabrica_de_tarro_carmesi += 5;
+    document.getElementById("fabricaTarroCosto").innerText = cst_fabrica_de_tarro_carmesi;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarLaboratorioNatilla(){
+  if(currentSalchipapas >= cst_laboratorio_de_natilla){
+    currentSalchipapas -= cst_laboratorio_de_natilla;
+    cant_laboratorio_de_natilla++;
+    cst_laboratorio_de_natilla += 5.5;
+    document.getElementById("laboratorioNatillaCosto").innerText = cst_laboratorio_de_natilla;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarFabricaTejidos(){
+  if(currentSalchipapas >= cst_fabrica_de_tejidos_extraños){
+    currentSalchipapas -= cst_fabrica_de_tejidos_extraños;
+    cant_fabrica_de_tejidos_extraños++;
+    cst_fabrica_de_tejidos_extraños += 5.5;
+    document.getElementById("FabricaTejidosCosto").innerText = cst_fabrica_de_tejidos_extraños;
+    updateProduccionPorSegundo();
+    updateUI();
+  }
+}
+
+function comprarMirella(){
+  if(currentSalchipapas >= cst_mirella_dimensional){
+    currentSalchipapas -= cst_mirella_dimensional;
+    cant_mirella_dimensional++;
+    cst_mirella_dimensional += 5.5;
+    document.getElementById("mirellaCosto").innerText = cst_mirella_dimensional;
     updateProduccionPorSegundo();
     updateUI();
   }
